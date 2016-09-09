173c2cee9b5610f5becc9ee32f8d3d3a969c31ff
Robin Murphy <robin@robinmurphy.co.uk>
2016-09-09 15:20:48 +0100

    Fix up statsd

--- statsd/config.js
+++ statsd/config.js
@@ -2,5 +2,12 @@
   graphitePort: 2003,
   graphiteHost: "graphite",
   port: 8125,
-  backends: ["./backends/graphite"]
+  backends: ["./backends/graphite"],
+  graphite: {
+    legacyNamespace: false
+  },
+  deleteGauges: false,
+  deleteCounters: true,
+  deleteTimers: true,
+  deleteSets: true
 }
